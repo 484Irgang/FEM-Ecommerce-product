9b0a942d50980cb21c358309e19a2a07a19e52eb
Guilherm Irgang <484gui@gmail.com>
2022-12-08 18:09:32 -0300

    Instalando as dependencias para usar o Vue, através do Vite

---
+++ Ecommerce/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  import Header from './components/Header.vue'
+
+  export default{
+    components: {
+      Header
+    }
+  }
+</script>
+
+<template>
+  <Header/>
+</template>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .container{
+    width: 100%;
+    max-width: 1200px;
+    padding: 0 2%;
+    margin: auto;
+  }
+</style>
---
+++ Ecommerce/src/components/Header.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<template>
+	<header>
+		<div class="container">
+			<div class="logo"></div>
+			<ul>
+				<li><a href="#">Collections</a></li>
+				<li><a href="#">Men</a></li>
+				<li><a href="#">Women</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			<div class="cart"></div>
+			<div class="img-profile"></div>
+		</div>
+	</header>
+</template>
+
+<style scoped>
+	header{
+		background: blue;
+	}
+</style>
